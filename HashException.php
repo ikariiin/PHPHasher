fbcd5deecc655bea88fa8afec9ae5be804f9da80
Gourab Nag <gourabnag12@gmail.com>
2015-02-25 14:55:28 +0530

    Provided member Functions to be called
    The Exception Class For Hasher.php

--- HashException.php
+++ HashException.php
@@ -1,6 +1,22 @@
 <?php
   class HashException extends Exception
   {
-    
+    protected $err_code;
+    protected $err_msg;
+    private function __constructor($error_code, $error_msg)
+    {
+      $this->err_code = $error_code;
+      $this->err_msg = $error_msg;
+    }
+    public function getMsg()
+    {
+      $formatted_err = "Error Code: " . $err_code . "; The Program Exited With Message: " . $err_message . ";";
+      return $formatted_err;
+    }
+    public function getErrCode()
+    {
+      $form_err_code = "Error Code: " . $err_code;
+      return $form_err_code;
+    }
   }
 ?>
