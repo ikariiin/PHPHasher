35806a9e83c58b31353b5b0442773359e19a3d12
Gourab Nag <gourabnag12@gmail.com>
2015-02-28 01:59:00 +0530

    Totally recreated the Hasher Class

--- Hasher.php
+++ Hasher.php
@@ -1,6 +1,45 @@
 <?php
-  class PHPHasher
+class Hasher
+{
+  private $present;
+  private $res_hash;
+  private $init_str;
+  private $algo;
+  private $err_msg;
+  private $msg;
+  private $salt;
+
+  public function __construct($init_str, $algo)
   {
-    
+    $this->init_str = $init_str;
+    if(in_array($algo, hash_algos()))
+    {
+      $this->algo = $algo;
+      $this->salt = rand();
+      $this->salt = hash("md5", $this->salt);
+      $joined = $this->init_str . $this->salt;
+      $res_hash = hash($this->algo, $joined);
+      $this->res_hash = $res_hash;
+    }
+    else
+    {
+      $this->err_msg = "Oppsie! The Hashing You Provided Does Not Exist!!!";
+    }
   }
+
+  public function getHash()
+  {
+    return $this->res_hash;
+  }
+
+  public function getSalt()
+  {
+    return $this->salt;
+  }
+
+  public function debug()
+  {
+    return $this->err_msg;
+  }
+}
 ?>
